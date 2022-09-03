4ae3a1539162240d039a7cd761f58f373ad76b21
Imtiaj09 <ahmimtiaj@gmail.com>
2022-09-03 21:47:52 +0600

    default menu bar and news category added

--- js/news-category.js
+++ js/news-category.js
@@ -2,11 +2,27 @@ const loadNewsCategory = async () => {
   const url = `https://openapi.programming-hero.com/api/news/categories`
   const res = await fetch(url)
   const data = await res.json()
-  displayNewsCategory(data.data.news_category[0].category_name)
+  const categorys = data.data.news_category;
+  const menuEategory = document.getElementById('menu-category');
+  for (const category of categorys) {
+    const newLi = document.createElement('li')
+    newLi.innerHTML = `
+    <a onclick="loadCardCategory('${category.category_id}')"class="nav-link text-dark" aria-current="page" href="#">${category.category_name}</a>
+    `
+    menuEategory.appendChild(newLi);
+
+  }
+
 }
 
-const displayNewsCategory = categorys => {
-  console.log(categorys)
+const loadCardCategory = async (categoryId) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+  const res = await fetch(url)
+  const data = await res.json()
+  const allNews = data.data
+  console.log(allNews)
 }
 
 loadNewsCategory()
+
+
