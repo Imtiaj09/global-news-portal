e5fc1e79dd3cb2bfbdf4be8efa78c2b0acf672a8
Imtiaj09 <ahmimtiaj@gmail.com>
2022-09-03 21:59:08 +0600

    css link added and style added

--- js/news-category.js
+++ js/news-category.js
@@ -20,9 +20,45 @@ const loadCardCategory = async (categoryId) => {
   const res = await fetch(url)
   const data = await res.json()
   const allNews = data.data
-  console.log(allNews)
+  const cardsContainer = document.getElementById('cards-container');
+  cardsContainer.innerHTML = '';
+  for (const news of allNews) {
+    const newCardDiv = document.createElement('div');
+    newCardDiv.classList.add("col")
+
+    newCardDiv.innerHTML = `
+    <div class="card">
+    <img src="${news.thumbnail_url}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${news.title}</h5>
+      <p class="card-text">
+        ${news.details.slice(0, 250)}...see more
+      </p>
+    </div>
+    <div class="card-footer">
+      <div class="d-flex align-items-center">
+        <div>
+          <img
+            class="author-profile rounded-circle"
+            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <h5>${news.author.name ? news.author.name : 'N/A'}</h5>
+          <h6>${news.author.published_date ? news.author.published_date.slice(0, 10) : 'N/A'}</h6>
+        </div>
+      </div>
+      <small class="text-muted"><i class="bi bi-eye"></i>${news.total_view ? news.total_view : 0}</small>
+      <button>serch</button>
+    </div>
+    </div>
+    `
+    cardsContainer.appendChild(newCardDiv);
+  }
 }
 
 loadNewsCategory()
 
+loadCardCategory()
 
